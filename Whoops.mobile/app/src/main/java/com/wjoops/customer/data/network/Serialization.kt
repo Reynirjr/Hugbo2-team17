a14bb5dd7b4b151bd311cc6c57058d0be89ea9a6
Benjamín <brj46@hi.is>
2026-02-09 14:33:58 +0000

    whoopsy

--- Whoops.mobile/app/src/main/java/com/wjoops/customer/data/network/Serialization.kt
+++
@@ -1,9 +0,0 @@
-package com.wjoops.customer.data.network
-
-import kotlinx.serialization.json.Json
-
-fun defaultJson(): Json = Json {
-    ignoreUnknownKeys = true
-    encodeDefaults = true
-    explicitNulls = false
-}
